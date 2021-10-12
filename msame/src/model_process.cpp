650a6824b7a367dbf81dea36fd0a2a055bb52889
chenjingliang <501339635@qq.com>
2021-10-12 06:59:25 +0000

    update msame/src/model_process.cpp.

--- msame/src/model_process.cpp
+++ msame/src/model_process.cpp
@@ -792,11 +792,11 @@ void ModelProcess::OutputModelResult(std::string& s, std::string& modelName, std
                 }
                 break;
             case 1:{
-		aclFloat16 * out_fp16 = reinterpret_cast<aclFloat16*>(outData);
-		float out = 0;
+                aclFloat16 * out_fp16 = reinterpret_cast<aclFloat16*>(outData);
+                float out = 0;
                 for (int i = 1; i <= len / sizeof(aclFloat16); i++) {
-		    out = aclFloat16ToFloat(out_fp16[i-1]);
-		    outstr << out << " ";
+		            out = aclFloat16ToFloat(out_fp16[i-1]);
+                    outstr << out << " ";
                     vector<int64_t>::iterator it;
                     for(it = curOutputDimsMul.begin(); it != curOutputDimsMul.end(); it++){
                         if ((i != 0) && (i % *it == 0)){
@@ -806,7 +806,7 @@ void ModelProcess::OutputModelResult(std::string& s, std::string& modelName, std
                     }                   
                 }
                 break;
-	    }
+	        }
             case 2:
                 for (int i = 1; i <= len / sizeof(int8_t); i++) {
                     int8_t out = *((int8_t*)outData + i - 1);
