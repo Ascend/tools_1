30ab3f50e0ac92180f865408baa72eec108ab7e2
lcm <liangchaoming@huawei.com>
2022-06-16 10:41:18 +0800

    更新inference_tool的name错误问题

--- ais-bench_workload/tool/inference_tools/backend/base/module/ModelInfer/ModelInferenceProcessor.cpp
+++ ais-bench_workload/tool/inference_tools/backend/base/module/ModelInfer/ModelInferenceProcessor.cpp
@@ -135,7 +135,7 @@ APP_ERROR ModelInferenceProcessor::CreateOutMemoryData(std::vector<MemoryData>& 
             ERROR_LOG("out i:%d size is zero custom:%d %d\n", size, customIndex, customOutTensorSize_.size());
             return APP_ERR_ACL_FAILURE;
         }
-        DEBUG_LOG("Create OutMemory i:%d name:%s size:%d\n", i, modelDesc_.outTensorsDesc[i],name.c_str(), size);
+        DEBUG_LOG("Create OutMemory i:%d name:%s size:%d\n", i, modelDesc_.outTensorsDesc[i].name.c_str(), size);
         Base::MemoryData memorydata(size, MemoryData::MemoryType::MEMORY_DEVICE, deviceId_);
         auto ret = MemoryHelper::MxbsMalloc(memorydata);
         if (ret != APP_ERR_OK) {
