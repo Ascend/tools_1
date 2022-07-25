cafbce0892c3a25ac185ccf85b9409caeea188fc
lanhaibo4 <lanhaibo4@h-partners.com>
2022-07-25 13:57:35 +0800

    [ais-bench_workload]优化save_tensors_to_file()函数

--- ais-bench_workload/tool/ais_infer/frontend/io_oprations.py
+++ ais-bench_workload/tool/ais_infer/frontend/io_oprations.py
@@ -171,7 +171,8 @@ def save_tensors_to_file(outputs, output_prefix, infiles_paths, outfmt, index):
                 file_path = os.path.join(output_prefix, "{}_{}.{}".format(
                     os.path.basename(infiles_perbatch[j][0]).split('.')[0], i, outfmt.lower()))
                 summary.add_sample_id_infiles(sample_id, infiles_perbatch[j])
-                logger.debug("save func: sampleid:{} infiles:{} out_{} file:{} fmt:{}".format(sample_id, i, infiles_perbatch[j], file_path, outfmt))
+                logger.debug("save func: sampleid:{} i:{} infiles:{} outfile:{} fmt:{}".format(
+                    sample_id, i, infiles_perbatch[j], file_path, outfmt))
                 summary.append_sample_id_outfile(sample_id, file_path)
                 save_data_to_files(file_path, subdata[j])
         else:
