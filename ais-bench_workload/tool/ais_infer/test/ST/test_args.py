cf1ee9437cd5ca6f4f7cb711491ac4a64172641a
lanhaibo4 <lanhaibo4@h-partners.com>
2022-07-26 21:45:44 +0800

    [ais-bench_workload]优化ais-infer自动化测试脚本

--- ais-bench_workload/tool/ais_infer/test/ST/test_args.py
+++ ais-bench_workload/tool/ais_infer/test/ST/test_args.py
@@ -6,7 +6,7 @@ import pytest
 class TestClass:
     def get_cmd_prefix(self):
         _current_dir = os.path.dirname(os.path.realpath(__file__))
-        return sys.executable + " " + os.path.join(_current_dir, " ../../ais_infer.py")
+        return sys.executable + " " + os.path.join(_current_dir, "../../ais_infer.py")
 
     def get_resnet_static_om_path(self, batchsize):
         _current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -15,22 +15,22 @@ class TestClass:
     def test_args_invalid_device_id(self):
         device_id = 100
         model_path = self.get_resnet_static_om_path(1)
-        cmd = "{} --model {} --device_id {}".format(self.get_cmd_prefix(), model_path, device_id)
+        cmd = "{} --model {} --device {}".format(self.get_cmd_prefix(), model_path, device_id)
         ret = os.system(cmd)
         assert ret != 0
 
     def test_args_invalid_model_path(self):
         device_id = 0
         model_path = "xxx_invalid.om"
-        cmd = "{} --model {} --device_id {}".format(self.get_cmd_prefix(), model_path, device_id)
+        cmd = "{} --model {} --device {}".format(self.get_cmd_prefix(), model_path, device_id)
         ret = os.system(cmd)
         assert ret != 0
 
     def test_args_invalid_acl_json(self):
         device_id = 0
         model_path = self.get_resnet_static_om_path(1)
-        acl_json_path="xxx_invalid.json"
-        cmd = "{} --model {} --device_id {}".format(self.get_cmd_prefix(), model_path, device_id)
+        acl_json_path = "xxx_invalid.json"
+        cmd = "{} --model {} --device {}".format(self.get_cmd_prefix(), model_path, device_id)
         cmd = "{} --acl_json_path {}".format(cmd, acl_json_path)
         ret = os.system(cmd)
         assert ret != 0
@@ -38,6 +38,10 @@ class TestClass:
     def test_args_ok(self):
         device_id = 0
         model_path = self.get_resnet_static_om_path(1)
-        cmd = "{} --model {} --device_id {}".format(self.get_cmd_prefix(), model_path, device_id)
+        cmd = "{} --model {} --device {}".format(self.get_cmd_prefix(), model_path, device_id)
+        print("cmd: {}".format(cmd))
+
+        with open('./log.txt', 'w') as f:
+            print("cmd: {}".format(cmd), file=f)
         ret = os.system(cmd)
         assert ret == 0
