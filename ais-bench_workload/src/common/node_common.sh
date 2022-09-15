6bbf670a5d4ad7b8bfe8b7be4286d667f7998538
lanhaibo4 <lanhaibo4@h-partners.com>
2022-09-15 14:41:35 +0800

    [ais-bench_workload]优化train_tool功能中RUN_PATH的相关代码

--- ais-bench_workload/src/common/node_common.sh
+++ ais-bench_workload/src/common/node_common.sh
@@ -92,10 +92,9 @@ function node_common_train()
             export DEVICE_INDEX=$DEVICE_ID
             export RANK_INDEX=$SERVER_ID
         fi
-        # clear and create path. it's  no need to delete and recreate RUN_PATH. because one train will be failed when
-        # two training action are exacuted almostly at the same time in container under the premise of calculation force segmentation
+        # clear and create path.
         RUN_PATH="$WORK_PATH/train_parallel$index"
-
+        mkdir -p $RUN_PATH; cd $RUN_PATH;
         # if bindcore should get cmdopt for cores
         [ $bindcore == "true" ] && { start=`expr $i \* $avg`; end=`expr $start \+ $gap`; cmdopt=$start"-"$end; }
         # call out func get run cmd
