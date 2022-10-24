411371a9272b64a9916e4cc27845f65029893873
lcm <liangchaoming@huawei.com>
2022-10-24 15:27:37 +0800

    更新devleop路径；修改h2D D2H的耗时info打印

--- ais-bench_workload/tool/ais_infer/ais_infer/summary.py
+++ ais-bench_workload/tool/ais_infer/ais_infer/summary.py
@@ -75,13 +75,13 @@ class Summary(object):
 
         # logger.debug("infer finish (ms) sumary:{}".format(self.infodict))
         logger.info("-----------------Performance Summary------------------")
-        logger.debug("H2D_latency (ms): min = {0}, max = {1}, mean = {2}, median = {3}, percentile({4}%) = {5}"
+        logger.info("H2D_latency (ms): min = {0}, max = {1}, mean = {2}, median = {3}, percentile({4}%) = {5}"
                     .format(h2d_latency.min, h2d_latency.max, h2d_latency.mean, h2d_latency.median, scale,
                             h2d_latency.percentile))
         logger.info("NPU_compute_time (ms): min = {0}, max = {1}, mean = {2}, median = {3}, percentile({4}%) = {5}"
                     .format(npu_compute_time.min, npu_compute_time.max, npu_compute_time.mean, npu_compute_time.median,
                             scale, npu_compute_time.percentile))
-        logger.debug("D2H_latency (ms): min = {0}, max = {1}, mean = {2}, median = {3}, percentile({4}%) = {5}"
+        logger.info("D2H_latency (ms): min = {0}, max = {1}, mean = {2}, median = {3}, percentile({4}%) = {5}"
                     .format(d2h_latency.min, d2h_latency.max, d2h_latency.mean, d2h_latency.median, scale,
                             d2h_latency.percentile))
         logger.info("throughput 1000*batchsize({})/NPU_compute_time.mean({}): {}".format(
