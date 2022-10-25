dca1b20cd596639a69282565d072984b9f155456
lanhaibo4 <lanhaibo4@h-partners.com>
2022-10-25 21:24:07 +0800

    [ais-bench_workload]推理工具增加参数auto_set_dymshape_modeST用例

--- ais-bench_workload/tool/ais_infer/test/ST/test_infer_resnet50.py
+++ ais-bench_workload/tool/ais_infer/test/ST/test_infer_resnet50.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import aclruntime
 import numpy as np
@@ -22,6 +23,7 @@ class TestClass():
         self.model_name = self.get_model_name(self)
         self.model_base_path = self.get_model_base_path(self)
         self.output_file_num = 5
+        self.auto_set_dymshape_mode_input_dir_path = os.path.join(self.model_base_path, "model", "input", "auto_set_dymshape_mode_input")
 
     def get_model_name(self):
         return "resnet50"
@@ -48,6 +50,18 @@ class TestClass():
 
     def get_dynamic_shape_om_path(self):
         return os.path.join(self.model_base_path, "model", "pth_resnet50_dymshape.om")
+
+    def create_npy_files_in_auto_set_dymshape_mode_input(self, shapes):
+        # default the folder is not existed
+        os.makedirs(self.auto_set_dymshape_mode_input_dir_path)
+
+        i = 1
+        for shape in shapes:
+            x = np.zeros(shape, dtype=np.int32)
+            file_name = 'input_shape_{}'.format(i)
+            file = os.path.join(self.auto_set_dymshape_mode_input_dir_path, "{}.npy".format(file_name))
+            np.save(file, x)
+            i += 1
 
     def test_pure_inference_normal_static_batch(self):
         """
@@ -104,39 +118,87 @@ class TestClass():
         ret = os.system(cmd)
         assert ret == 0
 
-    def test_pure_inference_normal_dynamic_shape_auto_set_dymshape_mode(self):
+    def test_inference_normal_dynamic_shape_auto_set_dymshape_mode(self):
+        """"
+        multiple npy input files as input parameter
+        """
         shapes = [[1, 3,  224,  224], [1, 3, 300, 300], [1, 3, 200, 200]]
-        num_shape = len(shapes)
+        if not os.path.exists(self.auto_set_dymshape_mode_input_dir_path):
+            self.create_npy_files_in_auto_set_dymshape_mode_input(shapes)
+        else:
+            filelist = os.listdir(self.auto_set_dymshape_mode_input_dir_path)
+            for file_path in filelist:
+                if not file_path.endswith(".npy"):
+                    os.remove(file_path)
+            if len(os.listdir(self.auto_set_dymshape_mode_input_dir_path)) == 0:
+                self.create_npy_files_in_auto_set_dymshape_mode_input(shapes)
+
         output_size = 10000
         model_path = self.get_dynamic_shape_om_path()
-        input_shapes = []
-        i = 1
-        for shape in shapes:
-            x = np.zeros(shape, dtype=np.int32)
-            file_name = 'save_{}'.format(i)
-            np.save(file_name, x)
-            file = "./{}.npy".format(file_name)
-            ndata = np.load(file)
-            input_shapes.append(file)
-            i += 1
-
-        shapes = ",".join(input_shapes)
-        output_dirname = "1"
-        cmd = "{} --model {} --device {} --outputSize {} --auto_set_dymshape_mode true --input {} --output ./  --output_dirname {} ".format(TestCommonClass.cmd_prefix, model_path,
-                                                                                TestCommonClass.default_device_id,
-                                                                                output_size, shapes, output_dirname)
+        filelist = os.listdir(self.auto_set_dymshape_mode_input_dir_path)
+        num_shape = len(filelist)
+        file_paths = []
+        for file in filelist:
+            file_paths.append(os.path.join(self.auto_set_dymshape_mode_input_dir_path, file))
+        file_paths = ",".join(file_paths)
+        output_parent_path = os.path.join(self.model_base_path, "model", "output")
+        output_dirname = "auto_set_dymshape_mode_output"
+        output_path = os.path.join(output_parent_path, output_dirname)
+        if os.path.exists(output_path):
+            shutil.rmtree(output_path)
+        os.makedirs(output_path)
+        cmd = "{} --model {} --device {} --outputSize {} --auto_set_dymshape_mode true --input {} --output {}  --output_dirname {} ".format(TestCommonClass.cmd_prefix, model_path,
+            TestCommonClass.default_device_id, output_size, file_paths, output_parent_path, output_dirname)
 
         ret = os.system(cmd)
         assert ret == 0
 
         try:
-            cmd = "find {} -name '*.bin'|wc -l".format("./{}".format(output_dirname))
+            cmd = "find {} -name '*.bin'|wc -l".format(output_path)
             bin_num = os.popen(cmd).read()
         except Exception as e:
             raise Exception("raise an exception: {}".format(e))
 
         assert int(bin_num) == num_shape
 
+    def test_inference_normal_dynamic_shape_auto_set_dymshape_mode_2(self):
+        """"
+        a folder containing multiple npy files as input parameter
+        """
+        shapes = [[1, 3,  224,  224], [1, 3, 300, 300], [1, 3, 200, 200]]
+        if not os.path.exists(self.auto_set_dymshape_mode_input_dir_path):
+            self.create_npy_files_in_auto_set_dymshape_mode_input(shapes)
+        else:
+            filelist = os.listdir(self.auto_set_dymshape_mode_input_dir_path)
+            for file_path in filelist:
+                if not file_path.endswith(".npy"):
+                    os.remove(file_path)
+            if len(os.listdir(self.auto_set_dymshape_mode_input_dir_path)) == 0:
+                self.create_npy_files_in_auto_set_dymshape_mode_input(shapes)
+
+        output_size = 10000
+        model_path = self.get_dynamic_shape_om_path()
+        filelist = os.listdir(self.auto_set_dymshape_mode_input_dir_path)
+        num_shape = len(filelist)
+        output_parent_path = os.path.join(self.model_base_path, "model", "output")
+        output_dirname = "auto_set_dymshape_mode_output"
+        output_path = os.path.join(output_parent_path, output_dirname)
+        if os.path.exists(output_path):
+            shutil.rmtree(output_path)
+        os.makedirs(output_path)
+        cmd = "{} --model {} --device {} --outputSize {} --auto_set_dymshape_mode true --input {} --output {}  --output_dirname {} ".format(TestCommonClass.cmd_prefix, model_path,
+            TestCommonClass.default_device_id, output_size, self.auto_set_dymshape_mode_input_dir_path, output_parent_path, output_dirname)
+
+        ret = os.system(cmd)
+        assert ret == 0
+
+        try:
+            cmd = "find {} -name '*.bin'|wc -l".format(output_path)
+            bin_num = os.popen(cmd).read()
+        except Exception as e:
+            raise Exception("raise an exception: {}".format(e))
+
+        assert int(bin_num) == num_shape
 
     def test_general_inference_normal_static_batch(self):
         batch_size = 1
