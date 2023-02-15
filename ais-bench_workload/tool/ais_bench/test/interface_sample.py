3571e2ac3caa9a4f705cace4b908fa0575ce8cf6
lanhaibo4 <lanhaibo4@h-partners.com>
2023-02-15 11:21:32 +0800

    [ais-bench_workload]添加interface_sample.py接口init()的用例

--- ais-bench_workload/tool/ais_bench/test/interface_sample.py
+++ ais-bench_workload/tool/ais_bench/test/interface_sample.py
@@ -89,8 +89,43 @@ def get_model_info():
         print("outputs info i:{} shape:{} type:{} val:{} realsize:{} size:{}".format(
             i, info.shape, info.datatype, int(info.datatype), info.realsize, info.size))
 
-infer_simple()
+def infer_init():
+    loop = 100
+    device_id = 0
+    exception_num = 0
+    for i in range(loop):
+        try:
+            session = InferSession(device_id, model_path)
+            del session
+        except Exception as e:
+            print("session finalize {} time, exception: {}".format(i + 1, e))
+            exception_num += 1
+
+    assert exception_num == 0
+
+def infer_finalize():
+    loop = 3
+    device_id = 0
+    exception_num = 0
+
+    for i in range(loop):
+        try:
+            session = InferSession(device_id, model_path)
+            barray = bytearray(session.get_inputs()[0].realsize)
+            ndata = np.frombuffer(barray)
+            outputs = session.infer([ndata])
+
+
+        except Exception as e:
+            print("session finalize {} time, exception: {}".format(i + 1, e))
+            exception_num += 1
+    session.finalize()
+    assert exception_num == 0
+
+# infer_simple()
 # infer_torch_tensor()
 #infer_dymshape()
 # infer_dymdims()
 #get_model_info()
+# infer_init()
+infer_finalize()
