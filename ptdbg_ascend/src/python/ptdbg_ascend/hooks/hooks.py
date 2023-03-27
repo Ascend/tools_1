f4b08891ed205384efdc05bf27c27d625dee69ab
yang-minghai22 <yangminghai2@huawei.com>
2023-03-27 10:10:21 +0800

    fix api dump mode bug

--- ptdbg_ascend/src/python/ptdbg_ascend/hooks/hooks.py
+++ ptdbg_ascend/src/python/ptdbg_ascend/hooks/hooks.py
@@ -296,7 +296,7 @@ def make_dump_data_dir(dump_file_name):
 
 def _set_dump_switch4api_list(name):
     if DumpUtil.dump_api_list:
-        api_name = name.rsplit("_", 3)[0].split("_", 1)[1].lower()
+        api_name = name.rsplit("_", 2)[0].split("_", 1)[1].lower()
         DumpUtil.dump_switch = "ON" if api_name in DumpUtil.dump_api_list else "OFF"
 
 
